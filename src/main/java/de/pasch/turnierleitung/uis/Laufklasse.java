133996c2d19da18ac9ab19e3d9c88f5e044a8910
paulsch <pa.schuetz@web.de>
2020-01-11 18:38:09 +0100

    .jar Datei kann erzeugt werden

---
+++ src/main/java/de/pasch/turnierleitung/uis/Laufklasse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.pasch.turnierleitung.uis;
+
+/**
+ *
+ * @author pasch
+ */
+public class Laufklasse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Startfenster.main(args);
+    }
+    
+}
